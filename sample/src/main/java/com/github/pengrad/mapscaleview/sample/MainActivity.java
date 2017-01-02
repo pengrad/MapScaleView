61ff136aaaeb5d98ebdc70a561a40406e667166a
Stas Parshin <s.a.parshin@gmail.com>
2017-01-02 21:20:34 +0700

    Update sample

--- sample/src/main/java/com/github/pengrad/mapscaleview/sample/MainActivity.java
+++ sample/src/main/java/com/github/pengrad/mapscaleview/sample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.github.pengrad.mapscaleview.sample;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.github.pengrad.mapscaleview.MapScaleView;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,10 +14,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener, GoogleMap.OnCameraIdleListener, GoogleMap.OnCameraChangeListener {
 
     private GoogleMap map;
     private MapScaleView scaleView;
+    private MapScaleView scaleViewMiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
         scaleView = (MapScaleView) findViewById(R.id.scaleView);
+        scaleViewMiles = (MapScaleView) findViewById(R.id.scaleViewMiles);
     }
 
     @Override
@@ -40,16 +47,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onCameraMove() {
-        scaleView.update(map.getProjection(), map.getCameraPosition());
+        update(map.getCameraPosition());
     }
 
     @Override
     public void onCameraIdle() {
-        scaleView.update(map.getProjection(), map.getCameraPosition());
+        update(map.getCameraPosition());
     }
 
     @Override
     public void onCameraChange(CameraPosition cameraPosition) {
-        scaleView.update(map.getProjection(), cameraPosition);
+        update(cameraPosition);
+    }
+
+    private void update(CameraPosition cameraPosition) {
+        scaleView.update(cameraPosition.zoom, cameraPosition.target.latitude);
+        scaleViewMiles.update(cameraPosition.zoom, cameraPosition.target.latitude);
+    }
+
+    public void changeColor(View view) {
+        scaleView.setColor(Color.parseColor("#991111"));
+    }
+
+    public void changeTextSize(View view) {
+        scaleView.setTextSize(18);
+    }
+
+    public void changeStrokeWidth(View view) {
+        scaleView.setStrokeWidth(2);
+    }
+
+    public void changeMiles(View view) {
+        scaleView.setIsMiles(true);
+    }
+
+    public void changeSize(View view) {
+        ViewGroup.LayoutParams layoutParams = scaleView.getLayoutParams();
+        layoutParams.width = new Random().nextBoolean() ? 400 : 200;
+        scaleView.requestLayout();
     }
 }
